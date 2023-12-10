42254004e23da6fa9d908874903d1988d39c36b4
dltjsrbii <dltjsrbii@gmail.com>
2023-12-10 21:26:09 +0900

    script+Resources

---
+++ Assets/Script/InvScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InvScript : MonoBehaviour
+{
+    [SerializeField]
+    Image InventoryC;
+
+    [SerializeField]
+    Image KeyC;
+
+    private bool invenOn = false;
+    private bool keyOn = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InventoryC.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            InventoryC.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            InventoryC.gameObject.SetActive(false);
+        }
+    }
+}
